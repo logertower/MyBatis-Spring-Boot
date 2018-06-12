a054ce0e2282225743c0543242a9e54ce963913f
xuxiaodong <xuxiaodong@gotokeep.com>
2018-06-12 23:33:14 +0800

    test

--- src/main/java/tk/mybatis/springboot/common/exception/ErrorInfos.java
+++ src/main/java/tk/mybatis/springboot/common/exception/ErrorInfos.java
@@ -25,6 +25,7 @@ public class ErrorInfos {
     }
 
     public static void main(String[] args) {
-        System.out.println("hello world");
+        System.out.println("Hello World");
+        System.out.println("nihao");
     }
 }
